1db39c7b888ebedfe019163672f4781b4595122d
mrOrlando <jurik48@xakep.ru>
2014-11-22 22:42:16 +0300

    Add exOr in bool.hs

--- topics/topic2/bool.hs
+++ topics/topic2/bool.hs
@@ -3,10 +3,29 @@ myNot True = False
 myNot False = True
 
 {-
-    sample in REPL:
-    :l bool
-    3 == 56-53
-    > True
-    myNot (3 == 56-53)
-    > False
+	sample in REPL:
+	:l bool
+	> 3 == 56-53
+	True
+	> myNot (3 == 56-53)
+	False
+-}
+
+
+-- exOr(True, False) -> True
+-- exOr(False, True) -> True
+-- exOr(_, _) -> False
+exOr :: (Bool, Bool) => Bool
+exOr x y = (x || y) && not (x && y)
+
+{-
+	:l bool
+	> exOr True False
+	True
+	> exOr False True
+	True
+	> exOr False False
+	False
+	> exOr True True
+	False
 -}
